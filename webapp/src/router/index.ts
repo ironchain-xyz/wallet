e2dc988aebdda57c7d1f9d14d825a3024a814c7b
ironchain-dao <ironchaindao@gmail.com>
2022-06-27 17:49:44 -0700

    do not use hash mode

--- webapp/src/router/index.ts
+++ webapp/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/HomeView.vue'
 import Discover from '../views/DiscoverView.vue'
 import Profile from '../views/ProfileView.vue'
@@ -60,7 +60,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(process.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 })
 
